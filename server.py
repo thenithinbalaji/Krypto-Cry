4933ee16ab6abdcee4c3b72228033f14abe2476b
thenithinbalaji <thenithinbalaji@gmail.com>
2023-05-31 00:43:57 +0530

    created flask landing page for the bot

--- server.py
+++ server.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template
 from threading import Thread
+import requests
+import os
+
+# loading env variables
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except Exception as err:
+    print(err)
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-	return render_template("index.html")
+    data = requests.get(str(os.getenv("coin_api"))).json()
+    return render_template("index.html", data=data)
 
 
 def run():
-	app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 
 def runserver():
-	thd = Thread(target=run)
-	thd.start()
+    thd = Thread(target=run)
+    thd.start()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
